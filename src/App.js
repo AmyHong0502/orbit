44335f8af524f002c58a55874c886a497d065d3a
Amy Hong <amyhong0502@gmail.com>
2019-01-06 14:30:42 -0800

    feat: pass data from navItem to Main on click

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Main from './components/Main';
 import NavBar from './components/NavBar';
 
-const App = () => {
-  return (
-    <div className='row index-bg'>
-      <NavBar />
-      <Main />
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    selectedMenu: null
+  };
+
+  onMenuItemSelect = menuItem => {
+    this.setState({
+      selectedMenu: menuItem
+    });
+  };
+
+  render() {
+    return (
+      <div className='row index-bg'>
+        <NavBar onMenuItemSelect={this.onMenuItemSelect} />
+        <Main selected={this.state.selectedMenu} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,12 +3,25 @@ import Data from './Data';
 
 class Main extends React.Component {
   render() {
+    if (this.props.selected) {
+      return (
+        <main role='main' class='col-md-9 ml-sm-auto col-lg-10 px-4 h-100'>
+          <div class='d-flex flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3'>
+            <h1 class='h2'>Main</h1>
+          </div>
+
+          <h2 className='text-white'>{this.props.selected.name}</h2>
+          <p>{this.props.selected.description}</p>
+          <Data />
+        </main>
+      );
+    }
+
     return (
       <main role='main' class='col-md-9 ml-sm-auto col-lg-10 px-4 h-100'>
         <div class='d-flex flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3'>
-          <h1 class='h2'>Main</h1>
+          Main
         </div>
-        <Data />
       </main>
     );
   }
