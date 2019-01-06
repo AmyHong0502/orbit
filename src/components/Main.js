f2efd592a0d4449a4838fe01443fed8eb69a0d90
Amy Hong <amyhong0502@gmail.com>
2019-01-06 14:40:08 -0800

    feat: connect API components with main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import Data from './Data';
+
+import Apod from '../apis/Apod';
+import NeoWs from '../apis/NeoWs';
 
 class Main extends React.Component {
+  chooseMenu = () => {
+    switch (this.props.selected.id) {
+      case 'Apod':
+        return <Apod />;
+      case 'NeoWs':
+        return <NeoWs />;
+      default:
+        return <div>Choose a menu</div>;
+    }
+  };
+
   render() {
-    if (this.props.selected) {
+    if (!this.props.selected) {
       return (
         <main role='main' className='col-md-9 ml-sm-auto col-lg-10 px-4 h-100'>
           <div className='d-flex flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3'>
-            <h1 className='h2'>Main</h1>
+            Main
           </div>
-
-          <h2 className='text-white'>{this.props.selected.name}</h2>
-          <p>{this.props.selected.description}</p>
-          <Data />
         </main>
       );
     }
@@ -20,8 +29,13 @@ class Main extends React.Component {
     return (
       <main role='main' className='col-md-9 ml-sm-auto col-lg-10 px-4 h-100'>
         <div className='d-flex flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3'>
-          Main
+          <h1 className='h2'>Main</h1>
         </div>
+
+        {this.chooseMenu()}
+
+        <h2 className='text-white'>{this.props.selected.name}</h2>
+        <p>{this.props.selected.description}</p>
       </main>
     );
   }
