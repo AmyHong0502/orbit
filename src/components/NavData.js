b838fb951ee2a7bfae1a41592a7e2c2616dc5017
Amy Hong <amyhong0502@gmail.com>
2019-01-06 14:29:26 -0800

    feat: add data for navigation bar

---
+++ src/components/NavData.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    id: 'Apod',
+    name: 'APOD',
+    description: 'Lorem Ipsum'
+  },
+  {
+    id: 'NeoWs',
+    name: 'NeoWs',
+    description: 'Dolor Sit Amet'
+  }
+];
