7a792a64e477a57c78eb1d3729f76ad3198dceb8
Amy Hong <amyhong0502@gmail.com>
2019-01-13 16:48:47 -0800

    feat: get EPIC API response

--- src/components/contents/Epic.js
+++ src/components/contents/Epic.js
@@ -1,8 +1,50 @@
+import { API_KEY } from '../../apis/credentials.json';
 import React, { Component } from 'react';
+import nasa from '../../apis/nasa';
 
 class Epic extends Component {
+  state = {
+    response: []
+  };
+
+  componentDidMount() {
+    this.loadApiData();
+  }
+
+  loadApiData = async () => {
+    const response = await nasa.get('/EPIC/api/natural', {
+      params: {
+        api_key: API_KEY
+      }
+    });
+
+    const data = response.data;
+
+    this.setState({
+      response: data
+    });
+  };
+
   render() {
-    return <div>Epic</div>;
+    return (
+      <div>
+        Epic
+        <ul>
+          <li>natural</li>
+          <ul>
+            <li>date</li>
+            <li>all</li>
+            <li>available</li>
+          </ul>
+          <li>enhanced</li>
+          <ul>
+            <li>date</li>
+            <li>all</li>
+            <li>available</li>
+          </ul>
+        </ul>
+      </div>
+    );
   }
 }
 
