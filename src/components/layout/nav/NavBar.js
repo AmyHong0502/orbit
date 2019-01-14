4dec633621e69352556b85192a4cc4c5fea2c1be
Amy Hong <amyhong0502@gmail.com>
2019-01-13 23:23:18 -0800

    feat: add satellite icon on NavBar

--- src/components/layout/nav/NavBar.js
+++ src/components/layout/nav/NavBar.js
@@ -17,6 +17,9 @@ const NavBar = ({ onMenuItemSelect, match }) => {
   return (
     <nav className='col-md-2 d-md-block sidebar p-0'>
       <div className='sidebar-sticky'>
+        <div className='navbar-brand p-3 m-0'>
+          <i className='fas fa-satellite fa-2x pr-2' />
+        </div>
         <ul className='nav flex-column'>{renderedList}</ul>
       </div>
     </nav>
