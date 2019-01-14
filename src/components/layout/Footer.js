09e2ec1c06938fb5ba86e360999034c011db8f13
Amy Hong <amyhong0502@gmail.com>
2019-01-13 22:36:13 -0800

    feat: add footer component

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <span className='text-muted'>Footer text goes here</span>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/layout/main/index.js
+++ src/components/layout/main/index.js
@@ -10,23 +10,31 @@ import Eonet from '../../contents/Eonet';
 import Earth from '../../contents/Earth';
 import ExoplanetArchive from '../../contents/ExoplanetArchive';
 import GeneLabSearch from '../../contents/GeneLabSearch';
+import Footer from '../Footer';
 
 class Main extends React.Component {
   render() {
     return (
-      <main role='main' className='col-md-10 ml-sm-auto px-4 h-100'>
-        <div className='d-flex flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3'>
-          <Route path='/' exact component={Home} />
-          <Route path='/apod' exact component={Apod} />
-          <Route path='/neows' exact component={NeoWs} />
-          <Route path='/donki' component={Donki} />
-          <Route path='/epic' exact component={Epic} />
-          <Route path='/eonet' exact component={Eonet} />
-          <Route path='/earth' exact component={Earth} />
-          <Route path='/exoplanet-archive' exact component={ExoplanetArchive} />
-          <Route path='/gene-lab' exact component={GeneLabSearch} />
-        </div>
-      </main>
+      <div id='content' className='col-md-10 ml-sm-auto px-4 h-100'>
+        <main role='main'>
+          <div className='d-flex flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3'>
+            <Route path='/' exact component={Home} />
+            <Route path='/apod' exact component={Apod} />
+            <Route path='/neows' exact component={NeoWs} />
+            <Route path='/donki' component={Donki} />
+            <Route path='/epic' exact component={Epic} />
+            <Route path='/eonet' exact component={Eonet} />
+            <Route path='/earth' exact component={Earth} />
+            <Route
+              path='/exoplanet-archive'
+              exact
+              component={ExoplanetArchive}
+            />
+            <Route path='/gene-lab' exact component={GeneLabSearch} />
+          </div>
+        </main>
+        <Footer />
+      </div>
     );
   }
 }
