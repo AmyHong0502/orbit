99c748e812fa724951bf79093f2f6df4fecb12fe
Amy Hong <amyhong0502@gmail.com>
2019-01-13 21:51:35 -0800

    feat: get CME data from NASA API

--- src/components/contents/Donki/Cme.js
+++ src/components/contents/Donki/Cme.js
@@ -1,6 +1,63 @@
+import { API_KEY } from '../../../apis/credentials';
 import React, { Component } from 'react';
+import nasa from '../../../apis/nasa';
 
 class Cme extends Component {
+  state = {
+    data: [
+      {
+        activeRegionNum: null,
+        activityID: null,
+        catalog: null,
+        cmeAnalyses: [
+          {
+            enlilList: null,
+            halfAngle: null,
+            isMostAccurate: null,
+            latitude: null,
+            levelOfData: null,
+            longitude: null,
+            note: null,
+            speed: null,
+            time21_5: null,
+            type: null
+          }
+        ],
+        instruments: [
+          {
+            displayName: null,
+            id: null
+          }
+        ],
+        linkedEvents: [
+          {
+            activityID: null
+          }
+        ],
+        note: null,
+        sourceLocation: null,
+        startTime: null
+      }
+    ]
+  };
+
+  componentDidMount() {
+    this.loadApiData();
+  }
+
+  loadApiData = async () => {
+    const response = await nasa.get('/DONKI/CME', {
+      params: {
+        api_key: API_KEY
+      }
+    });
+    const data = response.data;
+
+    this.setState({
+      data: data
+    });
+  };
+
   render() {
     return <div>CME</div>;
   }
