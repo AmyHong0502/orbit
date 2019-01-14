33431522ed6af196d1739b3d06c72c3cd2990d74
Amy Hong <amyhong0502@gmail.com>
2019-01-13 16:48:32 -0800

    feat: get EONET API response

--- src/components/contents/Eonet.js
+++ src/components/contents/Eonet.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
+import eonet from '../../apis/eonet';
 
 class Eonet extends Component {
+  state = {
+    response: null
+  };
+
+  componentDidMount() {
+    this.loadApiData();
+  }
+
+  loadApiData = async () => {
+    const response = await eonet.get('/layers');
+
+    const data = response.data;
+
+    this.setState({
+      response: data
+    });
+  };
+
   render() {
-    return <div>Eonet</div>;
+    return (
+      <div>
+        Eonet
+        <ul>
+          <li>events</li>
+          <li>categories</li>
+          <li>layers</li>
+        </ul>
+      </div>
+    );
   }
 }
 
