6032c549475954ca2f27f97849483d464eb15423
Amy Hong <amyhong0502@gmail.com>
2019-01-15 10:05:24 -0800

    feat: get and display IPS data

--- src/components/contents/Donki/Ips.js
+++ src/components/contents/Donki/Ips.js
@@ -1,8 +1,58 @@
+import { API_KEY } from '../../../apis/credentials';
 import React, { Component } from 'react';
+import RowItem from './RowItem';
+import nasa from '../../../apis/nasa';
 
 class Ips extends Component {
+  state = {
+    data: [
+      {
+        activityID: null,
+        catalog: null,
+        eventTime: null,
+        instruments: [
+          {
+            id: null,
+            displayName: null
+          }
+        ]
+      }
+    ]
+  };
+
+  componentDidMount() {
+    this.loadApiData();
+  }
+
+  loadApiData = async () => {
+    const response = await nasa.get('/DONKI/IPS', {
+      params: {
+        api_key: API_KEY
+      }
+    });
+    const data = response.data;
+
+    this.setState({
+      data: data
+    });
+
+    console.log(this.state.data);
+  };
+
+  renderList = () =>
+    this.state.data.map(item => {
+      return (
+        <div key={item.activityID} className='mb-5 mt-5 container'>
+          <RowItem name={'activityID'} content={item.activityID} />
+          <RowItem name={'catalog'} content={item.catalog} />
+          <RowItem name={'eventTime'} content={item.eventTime} />
+          <RowItem name={'instruments'} content={item.instruments} />
+        </div>
+      );
+    });
+
   render() {
-    return <div>Ips</div>;
+    return this.renderList();
   }
 }
 
