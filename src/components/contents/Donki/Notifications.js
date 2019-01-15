50a1bdf3911fafb9027d0f7a02aa0ae688f783c1
Amy Hong <amyhong0502@gmail.com>
2019-01-15 12:17:30 -0800

    feat: get and display notifications API data

--- src/components/contents/Donki/Notifications.js
+++ src/components/contents/Donki/Notifications.js
@@ -1,8 +1,58 @@
+import { API_KEY } from '../../../apis/credentials';
 import React, { Component } from 'react';
+import RowItem from './RowItem';
+import nasa from '../../../apis/nasa';
 
 class Notifications extends Component {
+  state = {
+    data: [
+      {
+        messageBody: null,
+        messageID: null,
+        messageIssueTime: null,
+        messageType: null,
+        messageURL: null
+      }
+    ]
+  };
+
+  componentDidMount() {
+    this.loadApiData();
+  }
+
+  loadApiData = async () => {
+    const response = await nasa.get('/DONKI/notifications', {
+      params: {
+        api_key: API_KEY
+      }
+    });
+    const data = response.data;
+
+    this.setState({
+      data: data
+    });
+  };
+
+  renderList = () =>
+    this.state.data.map(item => {
+      return (
+        <div key={item.messageID} className='mb-5 mt-5 container'>
+          <RowItem name={'messageID'} content={item.messageID} />
+          <RowItem name={'messageBody'} content={item.messageBody} />
+          <RowItem name={'messageIssueTime'} content={item.messageIssueTime} />
+          <RowItem name={'messageType'} content={item.messageType} />
+          <RowItem name={'messageURL'} content={item.messageURL} />
+        </div>
+      );
+    });
+
   render() {
-    return <div>Notifications</div>;
+    return (
+      <div>
+        <h1 className='display-4'>Notifications</h1>
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
