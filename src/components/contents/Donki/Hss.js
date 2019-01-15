dc6bc6d05ccbce84cca08ea1fae90a24ec9850d8
Amy Hong <amyhong0502@gmail.com>
2019-01-15 11:19:49 -0800

    feat: get and display HSS data

--- src/components/contents/Donki/Hss.js
+++ src/components/contents/Donki/Hss.js
@@ -1,8 +1,67 @@
+import { API_KEY } from '../../../apis/credentials';
 import React, { Component } from 'react';
+import RowItem from './RowItem';
+import nasa from '../../../apis/nasa';
 
 class Hss extends Component {
+  state = {
+    data: [
+      {
+        eventTime: null,
+        hssID: null,
+        instruments: [
+          {
+            displayName: null,
+            id: null
+          }
+        ],
+        linkedEvents: [
+          {
+            activityID: null
+          }
+        ]
+      }
+    ]
+  };
+
+  componentDidMount() {
+    this.loadApiData();
+  }
+
+  loadApiData = async () => {
+    const response = await nasa.get('/DONKI/HSS', {
+      params: {
+        api_key: API_KEY
+      }
+    });
+    const data = response.data;
+
+    this.setState({
+      data: data
+    });
+
+    console.log(this.state.data);
+  };
+
+  renderList = () =>
+    this.state.data.map(item => {
+      return (
+        <div key={item.hssID} className='mb-5 mt-5 container'>
+          <RowItem name={'hssID'} content={item.hssID} />
+          <RowItem name={'eventTime'} content={item.eventTime} />
+          <RowItem name={'instruments'} content={item.instruments} />
+          <RowItem name={'linkedEvents'} content={item.linkedEvents} />
+        </div>
+      );
+    });
+
   render() {
-    return <div>Hss</div>;
+    return (
+      <div>
+        <h1 className='display-4 mb-1'>Hight Speed Stream</h1>
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
