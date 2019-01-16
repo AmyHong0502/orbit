77bcac35e933741d0d0ed86dd08fd96231645028
Amy Hong <amyhong0502@gmail.com>
2019-01-15 18:15:59 -0800

    feat: add link to the root page on the navigation bar's satellite icon

--- src/components/layout/nav/NavBar.js
+++ src/components/layout/nav/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import NavItem from './NavItem';
 import NavData from './NavData';
 
@@ -17,8 +18,10 @@ const NavBar = ({ onMenuItemSelect, match }) => {
   return (
     <nav className='col-md-2 d-md-block sidebar p-0'>
       <div className='sidebar-sticky'>
-        <div className='navbar-brand p-3 m-0'>
-          <i className='fas fa-satellite fa-2x pr-2' />
+        <div className='navbar-brand p-3 m-0 mb-2 mt-1'>
+          <Link to='/'>
+            <i className='fas fa-satellite fa-2x pr-2' />
+          </Link>
         </div>
         <ul className='nav flex-column'>{renderedList}</ul>
       </div>
