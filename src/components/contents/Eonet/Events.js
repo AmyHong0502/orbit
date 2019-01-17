b2cb9814daf8c67a3f36bc829656c246b5b846f1
Amy Hong <amyhong0502@gmail.com>
2019-01-16 16:43:33 -0800

    feat: get EONET events API data

--- src/components/contents/Eonet/Events.js
+++ src/components/contents/Eonet/Events.js
@@ -1,12 +1,68 @@
 import React, { Component } from 'react';
 import Subtitle from './Subtitle';
+import eonet from '../../../apis/eonet';
 
 class Events extends Component {
+  state = {
+    data: {
+      title: null,
+      description: null,
+      events: [
+        {
+          id: null,
+          title: null,
+          categories: [
+            {
+              id: null,
+              title: null
+            }
+          ],
+          description: null,
+          geometries: [
+            {
+              coordinates: [
+                {
+                  0: null,
+                  1: null
+                }
+              ],
+              date: null,
+              type: null
+            }
+          ],
+          link: null,
+          sources: [
+            {
+              id: null,
+              url: null
+            }
+          ]
+        }
+      ],
+      link: null
+    }
+  };
+
+  componentDidMount() {
+    this.loadApiData();
+  }
+
+  loadApiData = async () => {
+    const response = await eonet.get('/events');
+
+    const data = response.data;
+    this.setState({
+      data: data
+    });
+  };
+
   render() {
     return (
       <div>
         <Subtitle text={'Events'} />
-        Events
+        <p>{this.state.data.title}</p>
+        <p>{this.state.data.description}</p>
+        <p>{this.state.data.link}</p>
       </div>
     );
   }
